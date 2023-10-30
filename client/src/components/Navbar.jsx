db275c8075b8d8ea77ec67ff9f5f7abeafecbe4d
BrianFuraha <maithabfrm@gmail.com>
2023-10-31 01:58:58 +0300

    side and nav done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,6 +10,8 @@ const Navbar = () => {
   const [isActive, setIsActive] = useState("dashboard");
   const [toggleDrawer, setToggleDrawer] = useState(false);
 
+  const address = "0xabc";
+
   return (
     <div className="flex md:flex-row flex-col-reverse justify-between mb-[35px] gap-6">
       <div className="lg:flex-1 flex flex-row max-w-[458px] py-2 pl-4 pr-2 h-[52px] bg-[#1c1c24] rounded-[100px]">
@@ -27,7 +29,93 @@ const Navbar = () => {
           />
         </div>
       </div>
-    ~</div>
+      <div className="sm:flex hidden flex-row justify-end gap-4">
+        <CustomButton
+          btnType="button"
+          title={address ? "Create a campaing" : "Connect"}
+          styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+          handleClick={() => {
+            if (address) navigate("create-campaing");
+            else "connect()";
+          }}
+        />
+
+        <Link to="/profile">
+          <div className="w-[52px] h-[52px] rounded-full bg-[#2c2f32] flex justify-center items-center cursor-pointer ">
+            <img
+              src={thirdweb}
+              alt="user"
+              className="w-[60%] h-[60%] object-contain "
+            />
+          </div>
+        </Link>
+      </div>
+
+      {/* small screen navigation */}
+      <div className="sm:hidden flex justify-between items-center relative">
+        <div className="w-[40px] h-[40px] rounded-[10px] bg-[#2c2f32] flex justify-center items-center cursor-pointer ">
+          <img
+            src={thirdweb}
+            alt="user"
+            className="w-[60%] h-[60%] object-contain "
+          />
+        </div>
+
+        <img
+          src={menu}
+          alt="menu"
+          className="w-[34px] h-[34px] object-contain cursor-pointer"
+          onClick={() => setToggleDrawer((prev) => !prev)}
+        />
+
+        <div
+          className={`absolute top-[60px] right-0 left-0 bg-[#1c1c24] z-10 shadow-secondary py-4 ${
+            !toggleDrawer ? "-translate-y-[100vh]" : "trnslate-y-0"
+          } transition-all duration-700`}
+        >
+          <ul className="mb-4">
+            {navlinks.map((Link) => (
+              <li
+                key={Link.name}
+                className={`flex p-4 ${isActive === Link.name && "bg-#3a3a43"}`}
+                onClick={() => {
+                  setIsActive(Link.name);
+                  setToggleDrawer(false);
+                  navigate(Link.link);
+                }}
+              >
+                <img
+                  src={Link.imgUrl}
+                  alt={Link.name}
+                  className={`w-[20px] h-[24px] object-contain ${
+                    isActive === Link.name ? "grayscale-0" : "grayscale"
+                  }`}
+                />
+                <p
+                  className={`ml-[20px] font-epilogue font-semibold text-[14px] ${
+                    isActive === Link.name ? "text-[#1dc071]" : "text-[#808191]"
+                  }`}
+                >
+                  {Link.name}
+                </p>
+              </li>
+            ))}
+          </ul>
+
+          <div className="flex mx-4">
+            <CustomButton
+              btnType="button"
+              title={address ? "Create a campaing" : "Connect"}
+              styles={address ? "bg-[#1dc071]" : "bg-[#8c6dfd]"}
+              handleClick={() => {
+                if (address) navigate("create-campaing");
+                else "connect()";
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
 
