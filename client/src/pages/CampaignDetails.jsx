bff1123188e0328203a6dfbdb5fc2e035e5dc3db
BrianFuraha <maithabfrm@gmail.com>
2024-01-29 23:45:07 +0300

    count box

--- client/src/pages/CampaignDetails.jsx
+++ client/src/pages/CampaignDetails.jsx
@@ -3,14 +3,69 @@ import { useLocation } from "react-router-dom";
 import { ethers } from "ethers";
 
 import { useStateContext } from "../context";
-import { CustomButton } from "../components";
+import { CountBox, CustomButton, Loader } from "../components";
 import { calculateBarPercentage, daysLeft } from "../utils";
 import { thirdweb } from "../assets";
 
 const CampaignDetails = () => {
   const { state } = useLocation();
-  console.log(state);
-  return <div>CampaingDetails</div>;
+  const { getDonations, contract, address } = useStateContext();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [amount, setAmount] = useState("");
+  const [donators, setDonators] = useState([]);
+
+  const remainingDays = daysLeft(state.deadline);
+
+  return (
+    <div>
+      {isLoading && "Loading..."}
+
+      <div className="w-full flex md:flex-row flex-col mt-10 gap-[30px]">
+        <div className="flex-1 flex-col">
+          <img
+            src={state.image}
+            alt="campaing"
+            className="w-full h-[410px] object-cover rounded-xl"
+          />
+          <div className="relative w-full h-[5px] bg-[#3a3a43] mt-2">
+            <div
+              className="absolute h-full bg-[#4acd8d]"
+              style={{
+                width: `${calculateBarPercentage(
+                  state.target,
+                  state.amountCollected
+                )}%`,
+                maxWidth: "100%",
+              }}
+            ></div>
+          </div>
+        </div>
+
+        <div className="flex md:w-[150px] w-full flex-wrap justify-between gap-[30px]">
+          <CountBox title="Days Left" value={remainingDays} />
+          <CountBox
+            title={`Raised of ${state.target}`}
+            value={state.amountCollected}
+          />
+          <CountBox title="Total Backers" value={donators.length} />
+        </div>
+      </div>
+
+      <div className="mt-[60px] flex lg:flex-row flex-col gap-5">
+        <div className="flex-[2] flex flex-col gap-[40px]">
+          <div>
+            <h4 className="font-epilogue font-semibold text=[18px] text-white uppercase">
+              Creator
+            </h4>
+            <div className="mt-[20px] flex flex-row items-center flex-wrap gap-[14px]">
+              <div className="w-[52px] h-[52px] flex items-center justify-center rounded-full bg-[#2c2f32] cursor-pointer"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CampaignDetails;
